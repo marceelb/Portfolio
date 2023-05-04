93486deb60fe2b45891b7de41a6ea616babb404c
Marcelo Brizuela <marceel.brizuela@gmail.com>
2023-05-04 16:39:14 -0300

    menu responsive

--- script/index.js
+++ script/index.js
@@ -1,7 +1,14 @@
 const root = document.querySelector('html');
 const switchBtn = document.querySelector('.switch-btn');
 const content = document.querySelector('.content');
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu2')
 
 switchBtn.addEventListener('click', () => {
     root.classList.toggle('dark');
-})
+});
+
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('menu2--show');
+});
